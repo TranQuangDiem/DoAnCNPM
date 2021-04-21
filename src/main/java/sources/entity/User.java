d8451d398a4397b956427cc8f280500593f713c0
tran diem <17130025@hcmuaf.edu.vn>
2021-04-21 19:45:07 +0700

    first commit

---
+++ src/main/java/sources/entity/User.java
@@ -0,0 +1,25 @@
+package sources.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User implements Serializable {
+    private static final long serialVersionUID = -297553281792804396L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String pass;
+    private String username;
+    private int phone;
+    private String address;
+    private boolean isLogin;
+    private boolean isActive;
+}
